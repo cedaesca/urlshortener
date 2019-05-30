d50f82c2873578578339084578950deb7894d469
César Escudero <cedaesca@gmail.com>
2019-05-30 19:45:41 -0400

    PSR-2 Coding style

--- src/Facades/URLShortener.php
+++ src/Facades/URLShortener.php
@@ -5,49 +5,28 @@ namespace cedaesca\URLShortener\Facades;
 use cedaesca\URLShortener\Models\ShortenedUrl;
 use Illuminate\Http\Request;
 
-class URLShortener {
-
-    /**
-     * URL's code length
-     *
-     * @var int
-     */
-    
-    public $length = 4;
-
-    /**
-     * If the code doesn't exist, it redirects to this default
-     *
-     * @var string
-     */
-
-    public $defaultRedirect = '/';
-
+class URLShortener 
+{
     /**
      * Genearet an unique code that will serve as an URL parameter
      *
      * @return string
      */
 
-    public function generateCode() {
-
+    public function generateCode(): string
+    {
         $extraCharacter = '';
 
-        if( $this->length % 2 != 0 ) {
-
+        if ($this->length % 2 != 0) {
             $extraCharacter = bin2hex( random_bytes( 4 ) );
             $extraCharacter = substr( $extraCharacter, -1 );
-
         }
 
         do {
-
             $code = bin2hex( random_bytes( $this->length / 2 ) ) . $extraCharacter;
+        } while ($this->checkIfCodeExists('shortlink', $code));
 
-        } while( $this->checkIfCodeExists( 'shortlink', $code ) );
-
-        return $code;
-        
+        return $code;        
     }
 
     /**
@@ -57,18 +36,15 @@ class URLShortener {
      * @return object
      */
 
-    static function redirect($code) {
-        
+    static function redirect($code) 
+    {
         $collection = ShortenedUrl::where('shortlink', $code)->first();
 
-        if( ! is_null( $collection ) ) {
-
+        if (!is_null($collection)) {
             return redirect($collection->target);
-
         }
 
         return redirect($this->defaultRedirect);
-
     }
 
      /**
@@ -79,18 +55,15 @@ class URLShortener {
      * @return boolean
      */
 
-    public function checkIfCodeExists($type, $value) {
-
+    public function checkIfCodeExists(string $type, string $value): bool 
+    {
         $collection = ShortenedUrl::where($type, $value)->first();
 
-        if( !is_null( $collection ) ) {
-
+        if (!is_null($collection)) {
             return true;
-
         }
 
         return false;
-
     }
 
     /**
@@ -100,27 +73,22 @@ class URLShortener {
      * @return cedaesca\URLShortener\Models\ShortenedUrl|boolean;
      */
 
-    static function create(Request $request) {
-
+    public static function create(Request $request) 
+    {
         $object = new URLShortener;
 
         $shortenedUrl = ShortenedUrl::create([
-
             'shortlink' => $object->generateCode(),
             'target' => $request->target,
             'title' => $request->title,
             'description' => $request->description
-
         ]);
 
-        if( $shortenedUrl->id ) {
-
+        if ($shortenedUrl->id) {
             return $shortenedUrl;
-
         }
 
         return false;
-
     }
 
     /**
@@ -130,20 +98,16 @@ class URLShortener {
      * @return cedaesca\URLShortener\Models\ShortenedUrl|boolean;
      */
 
-    static function update(Request $request) {
-
+    public static function update(Request $request) 
+    {
         $shortenedUrl = ShortenedUrl::where('id', $request->id);
         $shortenedUrl->title = $request->title;
         $shortenedUrl->description = $request->description;
         
-        if( $shortenedUrl->save() ) {
-
+        if ($shortenedUrl->save()) {
             return $shortenedUrl;
-
         }
 
         return false;
-
     }
-
 }
